2b8e684911334c7c22a7725a4b8dc9ab6ab5d58a
Sakib <sakibulanwar7@gmail.com>
2023-09-24 22:49:28 +0600

    group name fixed

--- backend/src/middlewares/jwt.middleware.ts
+++ backend/src/middlewares/jwt.middleware.ts
@@ -1,51 +1,65 @@
-import {Inject, Injectable, NestMiddleware, NotFoundException, UnauthorizedException} from '@nestjs/common';
-import {NextFunction, Request, Response} from 'express';
-import {verify} from 'jsonwebtoken';
-import {KEYCLOAK_DEFAULT_USER_ROLE, KEYCLOAK_PUBLIC_KEY, KEYCLOAK_REALM} from "../utils/constants";
-import {IdpService} from "../module/idp/idp.service";
-import {AuthUser} from "./user/auth-user";
+import {
+  Inject,
+  Injectable,
+  NestMiddleware,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import {
+  KEYCLOAK_DEFAULT_USER_GROUP,
+  KEYCLOAK_PUBLIC_KEY,
+  KEYCLOAK_REALM,
+} from '../utils/constants';
+import { IdpService } from '../module/idp/idp.service';
+import { AuthUser } from './user/auth-user';
 
 @Injectable()
 export class JwtMiddleware implements NestMiddleware {
-    @Inject()
-    private readonly idpService: IdpService;
+  @Inject()
+  private readonly idpService: IdpService;
 
-    async use(req: Request, res: Response, next: NextFunction) {
-        const adminToken = await this.idpService.getIdpToken(KEYCLOAK_REALM);
-        const token = req.headers.authorization?.split(' ')[1];
+  async use(req: Request, res: Response, next: NextFunction) {
+    const adminToken = await this.idpService.getIdpToken(KEYCLOAK_REALM);
+    const token = req.headers.authorization?.split(' ')[1];
 
-        if (!token) throw new UnauthorizedException("Login Required")
+    if (!token) throw new UnauthorizedException('Login Required');
 
-        const decoded = verify(token, KEYCLOAK_PUBLIC_KEY, {algorithms: ['RS256']})
-        const sub: any = decoded?.sub
+    const decoded = verify(token, KEYCLOAK_PUBLIC_KEY, {
+      algorithms: ['RS256'],
+    });
+    const sub: any = decoded?.sub;
 
-        const {
-            username,
-            firstName,
-            lastName,
-            email,
-            id
-        } = await this.idpService.getIdpUserBySub(KEYCLOAK_REALM, sub, adminToken)
+    const { username, firstName, lastName, email, id } =
+      await this.idpService.getIdpUserBySub(KEYCLOAK_REALM, sub, adminToken);
 
-        let user = {
-            id, username, firstName, lastName, email, roles: []
-        }
-        AuthUser.set(user)
+    let user = {
+      id,
+      username,
+      firstName,
+      lastName,
+      email,
+      roles: [],
+    };
+    AuthUser.set(user);
 
-        const allGroups = await this.idpService.getIdpGroups(adminToken)
+    const allGroups = await this.idpService.getIdpGroups(adminToken);
 
-        const userGroup = allGroups.filter((group) => group.name == KEYCLOAK_DEFAULT_USER_ROLE)
-        if (!userGroup) throw new NotFoundException('User Group not found')
+    const userGroup = allGroups.filter(
+      (group) => group.name == KEYCLOAK_DEFAULT_USER_GROUP,
+    );
+    if (!userGroup) throw new NotFoundException('User Group not found');
 
-        const userGroupId = userGroup[0].id
-        const groupRoles = await this.idpService.getIdpGroupRoles(userGroupId)
-        if (!groupRoles) throw new NotFoundException('Groups role not found')
+    const userGroupId = userGroup[0].id;
+    const groupRoles = await this.idpService.getIdpGroupRoles(userGroupId);
+    if (!groupRoles) throw new NotFoundException('Groups role not found');
 
-        const realmRole = groupRoles?.realmMappings[0]?.name;
-        // const clientRole = groupRoles?.clientMappings[KEYCLOAK_CLIENT_ID]?.mappings[0].name;
+    const realmRole = groupRoles?.realmMappings[0]?.name;
+    // const clientRole = groupRoles?.clientMappings[KEYCLOAK_CLIENT_ID]?.mappings[0].name;
 
-        AuthUser.addRole(realmRole)
+    AuthUser.addRole(realmRole);
 
-        next()
-    }
+    next();
+  }
 }
--- backend/src/module/auth/auth.service.ts
+++ backend/src/module/auth/auth.service.ts
@@ -1,69 +1,63 @@
-import {Inject, Injectable, NotFoundException} from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import axios from 'axios';
 import {
-    KEYCLOAK_CLIENT_ID,
-    KEYCLOAK_CLIENT_SECRET,
-    KEYCLOAK_DEFAULT_USER_ROLE,
-    KEYCLOAK_HOST_URL,
-    KEYCLOAK_REALM
-} from "../../utils/constants";
-import {AuthUser} from "../../middlewares/user/auth-user";
-import {HttpService} from "@nestjs/axios";
-import {IdpService} from "../idp/idp.service";
-
+  KEYCLOAK_CLIENT_ID,
+  KEYCLOAK_CLIENT_SECRET,
+  KEYCLOAK_DEFAULT_USER_GROUP,
+  KEYCLOAK_HOST_URL,
+  KEYCLOAK_REALM,
+} from '../../utils/constants';
+import { AuthUser } from '../../middlewares/user/auth-user';
+import { HttpService } from '@nestjs/axios';
+import { IdpService } from '../idp/idp.service';
 
 @Injectable()
 export class AuthService {
+  @Inject()
+  private httpService: HttpService;
+  private idpService: IdpService;
 
+  getKeycloakTokenURL = () => {
+    return `${KEYCLOAK_HOST_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token`;
+  };
 
-    @Inject()
-    private httpService: HttpService;
-    private idpService: IdpService;
+  async getKeycloakAuthToken(req: any, res: any) {
+    const params = {
+      grant_type: 'authorization_code',
+      client_id: KEYCLOAK_CLIENT_ID,
+      client_secret: KEYCLOAK_CLIENT_SECRET,
+      redirect_uri: req.body.redirect_uri,
+      code: req.body.code,
+    };
+    try {
+      const response = await axios.post(
+        this.getKeycloakTokenURL(),
+        new URLSearchParams(params),
+        {
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
 
-    getKeycloakTokenURL = () => {
-        return `${KEYCLOAK_HOST_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token`;
+      return res.status(200).json(response.data);
+    } catch (e: any) {
+      console.log('Error:', e.message);
+      throw new NotFoundException(e.message);
     }
+  }
 
-    async getKeycloakAuthToken(req: any, res: any) {
+  async getProfile() {
+    const user = AuthUser.get();
 
-        const params = {
-            grant_type: 'authorization_code',
-            client_id: KEYCLOAK_CLIENT_ID,
-            client_secret: KEYCLOAK_CLIENT_SECRET,
-            redirect_uri: req.body.redirect_uri,
-            code: req.body.code,
-        }
-        try {
+    const adminToken = await this.idpService.getIdpToken(KEYCLOAK_REALM);
+    // const allGroups=await this.idpService.getIdpGroups(adminToken)
+    // const defaultUserGroupId=allGroups.filter((group)=>group.name==KEYCLOAK_DEFAULT_USER_GROUP)
+    //
+    // console.log('default group =>', defaultUserGroupId)
 
-            const response = await axios.post(
-                this.getKeycloakTokenURL(),
-                new URLSearchParams(params),
-                {
-                    headers: {
-                        'content-type': 'application/x-www-form-urlencoded',
-                    },
-                },
-            );
-
-            return res.status(200).json(response.data);
-        } catch (e: any) {
-            console.log('Error:', e.message);
-            throw new NotFoundException(e.message);
-        }
-    }
-
-    async getProfile() {
-        const user = AuthUser.get()
-
-        const adminToken =await this.idpService.getIdpToken(KEYCLOAK_REALM);
-        // const allGroups=await this.idpService.getIdpGroups(adminToken)
-        // const defaultUserGroupId=allGroups.filter((group)=>group.name==KEYCLOAK_DEFAULT_USER_ROLE)
-        //
-        // console.log('default group =>', defaultUserGroupId)
-
-
-        console.log(user)
-        if (!user) throw new NotFoundException("Profile not found")
-        return user
-    }
+    console.log(user);
+    if (!user) throw new NotFoundException('Profile not found');
+    return user;
+  }
 }
